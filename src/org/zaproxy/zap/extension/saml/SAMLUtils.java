f51079ab51e2303ee936a975fde9504d02572b9e
pulasthi <pulasthi7@gmail.com>
2013-06-29 20:32:16 +0530

    Added documentation and fixed some error handling

--- src/org/zaproxy/zap/extension/saml/SAMLUtils.java
+++ src/org/zaproxy/zap/extension/saml/SAMLUtils.java
@@ -4,40 +4,73 @@ import org.parosproxy.paros.extension.encoder.Base64;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
+/**
+ * Contains some frequent methods related to decoding and encoding SAML messages
+ */
 public class SAMLUtils {
     private static final int MAX_INFLATED_SIZE = 5000;
 
-    public static byte[] b64Decode(String message) throws IOException {
-        return Base64.decode(message);
+    /**
+     * Base 64 decode a given string and gives the decoded data as a byte array
+     * @param message The String to base 64 decode
+     * @return Byte array of the decoded string
+     * @throws SAMLException
+     */
+    public static byte[] b64Decode(String message) throws SAMLException{
+        try {
+            return Base64.decode(message);
+        } catch (IOException e) {
+            throw new SAMLException("Base 64 Decode of failed for message: \n"+message,e);
+        }
     }
 
+    /**
+     * Base 64 encode the given byte array and gives the encoded string
+     * @param data The data to encode
+     * @return Encoded string
+     */
     public static String b64Encode(byte[] data){
         return Base64.encodeBytes(data);
     }
 
-    public static String inflateMessage(byte[] data) throws DataFormatException, UnsupportedEncodingException {
-        Inflater inflater = new Inflater(true);
-        inflater.setInput(data);
-        byte[] xmlMessageBytes = new byte[MAX_INFLATED_SIZE];
-        int resultLength = inflater.inflate(xmlMessageBytes);
+    /**
+     * Inflate a message (that had been deflated) and gets the original message
+     * @param data Byte array of deflated data that need to be inflated
+     * @return Original message after inflation
+     * @throws SAMLException
+     */
+    public static String inflateMessage(byte[] data) throws SAMLException {
+        try {
+            Inflater inflater = new Inflater(true);
+            inflater.setInput(data);
+            byte[] xmlMessageBytes = new byte[MAX_INFLATED_SIZE];
+            int resultLength = inflater.inflate(xmlMessageBytes);
 
-        if (!inflater.finished()) {
-            throw new RuntimeException("didn't allocate enough space to hold decompressed data");
+            if (!inflater.finished()) {
+                throw new SAMLException("Out of space allocated for inflated data");
+            }
+
+            inflater.end();
+
+            return new String(xmlMessageBytes, 0, resultLength,
+                    "UTF-8");
+        } catch (DataFormatException e) {
+            throw new SAMLException("Invalid data format",e);
+        } catch (UnsupportedEncodingException e) {
+            throw new SAMLException("Inflated data is not in valid encoding format",e);
         }
-
-        inflater.end();
-
-        String decodedResponse = new String(xmlMessageBytes, 0, resultLength,
-                "UTF-8");
-        return decodedResponse;
     }
 
-    public static String deflateMessage(String message){
-        return null;
+    /**
+     * Deflate a message to be send over a preferred binding
+     * @param message Message to be deflated
+     * @return The deflated message as a byte array
+     */
+    public static byte[] deflateMessage(String message){
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
 }
