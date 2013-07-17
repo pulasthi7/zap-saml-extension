11edeffeaf0611be6719a9f65aa82431a7854c3c
pulasthi <pulasthi7@gmail.com>
2013-07-17 09:56:00 +0530

    Added xmldoc builder code

--- src/org/zaproxy/zap/extension/saml/SAMLMessageWrapper.java
+++ src/org/zaproxy/zap/extension/saml/SAMLMessageWrapper.java
@@ -1,5 +1,20 @@
 package org.zaproxy.zap.extension.saml;
 
+import org.opensaml.Configuration;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.io.Unmarshaller;
+import org.opensaml.xml.io.UnmarshallerFactory;
+import org.opensaml.xml.io.UnmarshallingException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 public class SAMLMessageWrapper {
@@ -16,5 +31,32 @@ public class SAMLMessageWrapper {
 
     public Map<String, String> getAttributeMapping() {
         return attributeMapping;
+    }
+
+    private void extractAttributes(){
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setNamespaceAware(true);
+            DocumentBuilder docBuilder = null;
+            docBuilder = documentBuilderFactory.newDocumentBuilder();
+            ByteArrayInputStream is = new ByteArrayInputStream(originalMessage.getBytes("UTF-8"));
+
+            Document document = docBuilder.parse(is);
+            Element element = document.getDocumentElement();
+            UnmarshallerFactory unmarshallerFactory = Configuration.getUnmarshallerFactory();
+            Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(element);
+            XMLObject xmlObject = unmarshaller.unmarshall(element);
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (UnmarshallingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
