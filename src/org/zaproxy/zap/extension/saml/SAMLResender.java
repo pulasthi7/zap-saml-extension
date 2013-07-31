856cfd26b2ea8dd983bc316f7267f9b91f039ef0
pulasthi <pulasthi7@gmail.com>
2013-08-01 00:55:36 +0530

    Added code to add history items for resent requests

--- src/org/zaproxy/zap/extension/saml/SAMLResender.java
+++ src/org/zaproxy/zap/extension/saml/SAMLResender.java
@@ -1,11 +1,16 @@
 package org.zaproxy.zap.extension.saml;
 
+import org.parosproxy.paros.control.Control;
+import org.parosproxy.paros.extension.history.ExtensionHistory;
+import org.parosproxy.paros.model.HistoryReference;
 import org.parosproxy.paros.model.Model;
 import org.parosproxy.paros.network.HtmlParameter;
 import org.parosproxy.paros.network.HttpMessage;
 import org.parosproxy.paros.network.HttpSender;
 
+import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -48,13 +53,36 @@ public class SAMLResender {
      * Resend the message to the desired endpoint and get the response
      * @param msg The message to be sent
      */
-    public static void resendMessage(HttpMessage msg){
+    public static void resendMessage(final HttpMessage msg){
         HttpSender sender = new HttpSender(Model.getSingleton().getOptionsParam().getConnectionParam(),true,
                 HttpSender.MANUAL_REQUEST_INITIATOR);
         try {
             sender.sendAndReceive(msg);
+            EventQueue.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    if (!msg.getResponseHeader().isEmpty()) {
+                        final ExtensionHistory extension = (ExtensionHistory) Control.getSingleton()
+                                .getExtensionLoader().getExtension(ExtensionHistory.NAME);
+
+                        final int finalType = HistoryReference.TYPE_MANUAL;
+                        final Thread t = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                extension.addHistory(msg,
+                                        finalType);
+                            }
+                        });
+                        t.start();
+                    }
+                }
+            });
         } catch (IOException e) {
             //todo handle
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 }
