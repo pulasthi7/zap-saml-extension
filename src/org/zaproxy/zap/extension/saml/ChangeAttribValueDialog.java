6a26b7db9f00acc60058a0baabf4d7a284ee0202
pulasthi <pulasthi7@gmail.com>
2013-08-04 13:44:09 +0530

    Added "Change attribute value" dialog

---
+++ src/org/zaproxy/zap/extension/saml/ChangeAttribValueDialog.java
@@ -0,0 +1,67 @@
+package org.zaproxy.zap.extension.saml;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class ChangeAttribValueDialog extends JDialog {
+
+	private final JScrollPane contentPanel = new JScrollPane();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ChangeAttribValueDialog dialog = new ChangeAttribValueDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ChangeAttribValueDialog() {
+		setTitle("Add/Edit values");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JLabel lblHeader = new JLabel("One value per line");
+			contentPanel.setColumnHeaderView(lblHeader);
+		}
+		{
+			JTextArea textAreaValues = new JTextArea();
+			contentPanel.setViewportView(textAreaValues);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
