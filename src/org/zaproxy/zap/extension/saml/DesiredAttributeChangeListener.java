fcf1d0b325f96923af39503ac67a58483bc44690
pulasthi <pulasthi7@gmail.com>
2013-08-09 00:05:42 +0530

    Fixed a typo in the class

--- src/org/zaproxy/zap/extension/saml/DesiredAttributeChangeListener.java
+++ src/org/zaproxy/zap/extension/saml/DesiredAttributeChangeListener.java
@@ -2,7 +2,7 @@ package org.zaproxy.zap.extension.saml;
 
 import java.util.Set;
 
-public interface DesiredAttributeChangeListner {
+public interface DesiredAttributeChangeListener {
     void onDesiredAttributeValueChange(String attribute, String value);
     void onAddDesiredAttribute(String attribute, String values);
     void onDeleteDesiredAttribute(String attribute);
