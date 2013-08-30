064b5b7dd14ebfb6910ec1687c134e045373b5fd
pulasthi <pulasthi7@gmail.com>
2013-08-31 00:26:15 +0530

    added in-memory update for saml-attributes

--- src/org/zaproxy/zap/extension/saml/SAMLMessageChanger.java
+++ src/org/zaproxy/zap/extension/saml/SAMLMessageChanger.java
@@ -4,18 +4,23 @@ import org.joda.time.DateTime;
 import org.parosproxy.paros.network.HtmlParameter;
 import org.parosproxy.paros.network.HttpMessage;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Map;
 import java.util.Set;
 
@@ -123,25 +128,6 @@ public class SAMLMessageChanger {
     }
 
     /**
-     * Change the attribute value to the given new value
-     * @param attributeName name of the attribute to be changed
-     * @param value new value to be changed to
-     * @return whether the change is successful
-     */
-    public boolean changeAttributeValueTo(String attributeName, String value){
-        if(attributeMap.containsKey(attributeName)){
-            Attribute attribute = attributeMap.get(attributeName);
-            Object newValue = validateValueType(attribute.getValueType(), value);
-            if(newValue!=null){
-                attribute.setValue(newValue);
-                messageChanged = true;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
      * Check whether the values are applicable to the selected attribute
      * @param type
      * @param value
@@ -164,6 +150,60 @@ public class SAMLMessageChanger {
         } catch (NumberFormatException e) {
             return null;
         }
+    }
+
+    private void updateXMLDocument(){
+        XPathFactory xFactory = XPathFactory.newInstance();
+        XPath xpath = xFactory.newXPath();
+        for (Attribute attribute : attributeMap.values()) {
+            try {
+                NodeList nodeList = (NodeList) xpath.compile(attribute.getxPath()).evaluate(xmlDocument, XPathConstants.NODESET);
+                nodeList.item(0).setNodeValue(attribute.getValue().toString());
+            } catch (XPathExpressionException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+    }
+
+    private void updateMessage(){
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(xmlDocument), new StreamResult(writer));
+            samlMessageString = writer.getBuffer().toString().replaceAll("\n|\r", "");
+        } catch (TransformerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    /**
+     * Change the attribute value to the given new value
+     * @param attributeName name of the attribute to be changed
+     * @param value new value to be changed to
+     * @return whether the change is successful
+     */
+    public boolean changeAttributeValueTo(String attributeName, String value){
+        if(attributeMap.containsKey(attributeName)){
+            Attribute attribute = attributeMap.get(attributeName);
+            Object newValue = validateValueType(attribute.getValueType(), value);
+            if(newValue!=null){
+                attribute.setValue(newValue);
+                messageChanged = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public HttpMessage getChangedMessege(){
+        if(!messageChanged){
+            return httpMessage;
+        }else{
+            //build the new message
+        }
+        return httpMessage;
     }
 
 }
