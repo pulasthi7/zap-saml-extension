d6c8c756bf6d4022e77593f725fa7015692fbd7e
pulasthi <pulasthi7@gmail.com>
2013-08-31 18:59:36 +0530

    refactored existing default configuration data file

--- src/org/zaproxy/zap/extension/saml/SAMLConfigData.java
+++ src/org/zaproxy/zap/extension/saml/SAMLConfigData.java
@@ -1,12 +1,21 @@
 package org.zaproxy.zap.extension.saml;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Set;
 
 @XmlRootElement(namespace = "org.zaproxy.zap.extension.saml")
 public class SAMLConfigData {
     private boolean autoChangerEnabled;
+    private boolean xswEnabled;
+
+    @XmlElementWrapper(name = "AllAttributes")
+    @XmlElement(name = "Attribute")
     private Set<Attribute> availableAttributes;
+
+    @XmlElementWrapper(name = "AutoChangeAttributes")
+    @XmlElement(name = "Attribute")
     private Set<Attribute> autoChangeValues;
 
     public boolean isAutoChangerEnabled() {
