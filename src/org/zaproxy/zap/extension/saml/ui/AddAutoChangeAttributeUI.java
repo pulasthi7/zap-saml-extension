bbef1f787100a7808a772357b2cc9b75e3fc6058
pulasthi <pulasthi7@gmail.com>
2013-09-28 22:30:08 +0530

    changed styling of AddAutoChangeAttributeUI

--- src/org/zaproxy/zap/extension/saml/ui/AddAutoChangeAttributeUI.java
+++ src/org/zaproxy/zap/extension/saml/ui/AddAutoChangeAttributeUI.java
@@ -14,11 +14,6 @@ public class AddAutoChangeAttributeUI extends JDialog {
     private JComboBox<Attribute> comboBoxAttribSelect;
     private JTextField txtAttribValues;
 
-    public static void main(String[] args) {
-        AddAutoChangeAttributeUI ui = new AddAutoChangeAttributeUI(null);
-        ui.setVisible(true);
-    }
-
     /**
      * Create the dialog.
      */
@@ -29,78 +24,74 @@ public class AddAutoChangeAttributeUI extends JDialog {
         JPanel contentPanel = new JPanel();
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
-        contentPanel.setLayout(new BorderLayout(0, 0));
-        {
-            JPanel attribNamePanel = new JPanel();
-            FlowLayout flowLayout = (FlowLayout) attribNamePanel.getLayout();
-            flowLayout.setHgap(0);
-            flowLayout.setAlignment(FlowLayout.LEFT);
-            contentPanel.add(attribNamePanel, BorderLayout.NORTH);
-            {
-                attribNamePanel.setBorder(BorderFactory.createTitledBorder(SamlI18n.getMessage("saml.addchangeattrib.attribname")));
-                comboBoxAttribSelect = new JComboBox<>();
-                for (Attribute attribute : SAMLConfiguration.getInstance().getAvailableAttributes()) {
-                    if (!listener.getDesiredAttributes().contains(attribute)) {
-                        comboBoxAttribSelect.addItem(attribute.createCopy());
-                    }
+        contentPanel.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        JLabel lblName = new JLabel(SamlI18n.getMessage("saml.addchangeattrib.attribname"));
+
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(10,0,10,0);
+
+        contentPanel.add(lblName,c);
+
+        comboBoxAttribSelect = new JComboBox<>();
+        for (Attribute attribute : SAMLConfiguration.getInstance().getAvailableAttributes()) {
+            if (!listener.getDesiredAttributes().contains(attribute)) {
+                comboBoxAttribSelect.addItem(attribute);
+            }
+        }
+        comboBoxAttribSelect.setMaximumRowCount(5);
+
+        c.gridx++;
+        contentPanel.add(comboBoxAttribSelect,c);
+
+        c.gridy++;
+        c.gridx = 0;
+        JLabel lblValue = new JLabel(SamlI18n.getMessage("saml.addchangeattrib.attribvalue"));
+        contentPanel.add(lblValue,c);
+
+        c.gridx++;
+        txtAttribValues = new JTextField();
+        contentPanel.add(txtAttribValues, c);
+
+        JPanel footerPanel = new JPanel();
+        footerPanel.setLayout(new FlowLayout());
+        getContentPane().add(footerPanel,BorderLayout.SOUTH);
+
+
+        final JButton okButton = new JButton(SamlI18n.getMessage("saml.addchangeattrib.btn.ok"));
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (comboBoxAttribSelect.getSelectedItem() == null) {
+                    View.getSingleton().showWarningDialog(SamlI18n.getMessage("saml.addchangeattrib.msg.attribnotselected"));
+                    return;
                 }
-                comboBoxAttribSelect.setMaximumRowCount(5);
-                attribNamePanel.add(comboBoxAttribSelect);
-            }
-        }
-        {
-            JPanel attribValuesPanel = new JPanel();
-            contentPanel.add(attribValuesPanel, BorderLayout.CENTER);
-            attribValuesPanel.setLayout(new BorderLayout(5, 5));
-            {
-                JLabel lblAttributeValues = new JLabel(SamlI18n.getMessage("saml.addchangeattrib.attribvalue"));
-                lblAttributeValues.setHorizontalAlignment(SwingConstants.LEFT);
-                attribValuesPanel.add(lblAttributeValues, BorderLayout.NORTH);
-            }
-            {
-                {
-                    txtAttribValues = new JTextField();
-                    attribValuesPanel.add(txtAttribValues, BorderLayout.CENTER);
+                if (txtAttribValues.getText().equals("")) {
+                    JOptionPane.showMessageDialog(AddAutoChangeAttributeUI.this, SamlI18n.getMessage("saml.addchangeattrib.msg.novalue"), SamlI18n.getMessage("saml.addchangeattrib.msg.valueerror"), JOptionPane.ERROR_MESSAGE);
+                    return;
                 }
+                Attribute attribute = ((Attribute) comboBoxAttribSelect.getSelectedItem()).createCopy();
+                attribute.setValue(txtAttribValues.getText());
+                listener.onAddDesiredAttribute(attribute);
+                AddAutoChangeAttributeUI.this.setVisible(false);
             }
-        }
-        {
-            final JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                final JButton okButton = new JButton(SamlI18n.getMessage("saml.addchangeattrib.btn.ok"));
-                okButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        if (comboBoxAttribSelect.getSelectedItem() == null) {
-                            View.getSingleton().showWarningDialog(SamlI18n.getMessage("saml.addchangeattrib.msg.attribnotselected"));
-                            return;
-                        }
-                        if (txtAttribValues.getText().equals("")) {
-                            JOptionPane.showMessageDialog(AddAutoChangeAttributeUI.this, SamlI18n.getMessage("saml.addchangeattrib.msg.novalue"), SamlI18n.getMessage("saml.addchangeattrib.msg.valueerror"), JOptionPane.ERROR_MESSAGE);
-                            return;
-                        }
-                        Attribute attribute = ((Attribute) comboBoxAttribSelect.getSelectedItem()).createCopy();
-                        attribute.setValue(txtAttribValues.getText());
-                        listener.onAddDesiredAttribute(attribute);
-                        AddAutoChangeAttributeUI.this.setVisible(false);
-                    }
-                });
-                buttonPane.add(okButton);
-                getRootPane().setDefaultButton(okButton);
+        });
+        footerPanel.add(okButton);
+        getRootPane().setDefaultButton(okButton);
+
+        JButton cancelButton = new JButton(SamlI18n.getMessage("saml.addchangeattrib.btn.cancel"));
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddAutoChangeAttributeUI.this.setVisible(false);
             }
-            {
-                JButton cancelButton = new JButton(SamlI18n.getMessage("saml.addchangeattrib.btn.cancel"));
-                cancelButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        AddAutoChangeAttributeUI.this.setVisible(false);
-                    }
-                });
-                buttonPane.add(cancelButton);
-            }
-        }
+        });
+        footerPanel.add(cancelButton);
+
     }
 
     /**
